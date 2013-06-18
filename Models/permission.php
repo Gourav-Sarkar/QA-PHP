255bb5257ed385ee6c30bf1fd22083bb08b47a93
Gourav Sarkar <Gourav Sarkar@GS-DarkDevil-13>
2013-06-18 08:02:51 +0530

    Permission fixed

--- Models/permission.php
+++ Models/permission.php
@@ -2,7 +2,7 @@
 
 /*
  * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor.s
  * 
  */
 require_once 'models/ResourceStorage.php';
